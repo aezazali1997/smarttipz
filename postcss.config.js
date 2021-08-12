916a145743b58fae51b9e8be1612176cef9c650e
Muhammad Suleman <mousuleman@gmail.com>
2021-08-12 18:22:50 +0500

    postcss and tailwind settings update

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    plugins: {
-        tailwindcss: { config: './tailwindcss-config.js' },
-    },
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,11 @@
 module.exports = {
-    purge: {
-        content: [],
-    },
-    darkMode: false,
-    theme: {
-        screens: {
-            'sm': '640px',
-            // => @media (min-width: 640px) { ... }
-
-            'md': '768px',
-            // => @media (min-width: 768px) { ... }
-
-            'lg': '1024px',
-            // => @media (min-width: 1024px) { ... }
-
-            'xl': '1280px',
-            // => @media (min-width: 1280px) { ... }
-
-            '2xl': '1536px',
-            // => @media (min-width: 1536px) { ... }
-        },
-        extend: {},
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
