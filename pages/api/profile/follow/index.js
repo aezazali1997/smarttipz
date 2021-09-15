0c2988ac7c7895b3f30d30e6954902793540209e
Naveed Ahsan <naveedahsan755@hotmail.com>
2021-09-15 10:01:28 +0500

    fixed follwing issues

--- pages/api/profile/follow/index.js
+++ pages/api/profile/follow/index.js
@@ -2,42 +2,42 @@ const User = require('../../../../models/User');
 const jwt = require('jsonwebtoken');
 
 const handler = async (req, res) => {
-    if (req.method === 'GET') {
-        try {
-            if (!req.headers.authorization) {
-                return res.status(401).send({ error: true, data: [], message: 'Please Login' })
-            }
-            const { username } = jwt.verify(
-                req.headers.authorization.split(' ')[1],
-                process.env.SECRET_KEY
-            );
+  if (req.method === 'GET') {
+    try {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ error: true, data: [], message: 'Please Login' });
+      }
+      const { username } = jwt.verify(
+        req.headers.authorization.split(' ')[1],
+        process.env.SECRET_KEY
+      );
 
-            const user = await User.findOne({
-                attributes: ['id'],
-                where: { username }
-            });
-            if (!user) {
-                return res.status(404).send({ error: true, data: [], message: 'User Not Found' })
-            }
+      const user = await User.findOne({
+        attributes: ['id'],
+        where: { username }
+      });
+      if (!user) {
+        return res.status(404).send({ error: true, data: [], message: 'User Not Found' });
+      }
 
-            const followers = await user.getFollower();
+      const followers = await user.getFollower();
 
-            const followed = await user.getFollowed();
+      const followed = await user.getFollowed();
 
-            res.status(200).json({
-                error: false,
-                data: {
-                    followers: followers,
-                    followed: followed
-                },
-                message: 'Data Fetched Successfully'
-            });
-        } catch (err) {
-            res.status(500).send({ error: true, data: [], message: err.message });
-        }
-    } else {
-        res.status(404).end('Page Not Found');
+      res.status(200).json({
+        error: false,
+        data: {
+          followers: followers,
+          followed: followed
+        },
+        message: 'Data Fetched Successfully'
+      });
+    } catch (err) {
+      res.status(500).send({ error: true, data: [], message: err.message });
     }
+  } else {
+    res.status(404).end('Page Not Found');
+  }
 };
 
 export default handler;
--- pages/api/profile/set-follows.js
+++ pages/api/profile/set-follows.js
@@ -4,67 +4,64 @@ const User = require('../../../models/User');
 const jwt = require('jsonwebtoken');
 
 const handler = async (req, res) => {
-    if (req.method === 'POST') {
-        const {
-            headers,
-            body,
-            body: { username: otheruser }
-        } = req;
+  if (req.method === 'POST') {
+    const { headers, body } = req;
 
-        try {
-            if (!headers.authorization) {
-                return res.status(401).send({ error: true, data: [], message: 'Please Login' })
-            }
-            const { username } = jwt.verify(
-                headers.authorization.split(' ')[1],
-                process.env.SECRET_KEY
-            );
+    try {
+      if (!headers.authorization) {
+        return res.status(401).send({ error: true, data: [], message: 'Please Login' });
+      }
+      const { username } = jwt.verify(headers.authorization.split(' ')[1], process.env.SECRET_KEY);
 
-            const user = await User.findOne({
-                attributes: ['id'],
-                where: { username }
-            });
-            if (!user) {
-                return res.status(404).send({ error: true, data: [], message: 'User Not Found' })
-            }
+      const user = await User.findOne({
+        attributes: ['id'],
+        where: { username }
+      });
+      if (!user) {
+        return res.status(404).send({ error: true, data: [], message: 'User Not Found' });
+      }
 
-            const otherUser = await User.findOne({ attributes: ['id'], where: { username: otheruser } });
+      const otherUser = await User.findOne({
+        attributes: ['id'],
+        where: { username: body.otheruser }
+      });
 
-            if (!otherUser) {
-                return res.status(404).json({ error: true, message: 'Other User Not Found', data: [] });
-            }
+      if (!otherUser) {
+        return res.status(404).json({ error: true, message: 'Other User Not Found', data: [] });
+      }
 
-            console.log(otherUser);
-            const follower = await user.getFollower({ where: { followedId: otherUser.id } });
-            const followed = await user.getFollowed({ where: { followingId: user.id } });
-            console.log('alreadyFollowed: ', follower);
+      const followings = await user.getFollowed();
+      const following = followings.find((follow) => follow.id === otherUser.id);
 
-            if (!isEmpty(follower) || !isEmpty(followed)) {
-                await user.setFollowed(otherUser);
-                await otherUser.setFollower(user);
-                res.status(201).send({
-                    error: false,
-                    message: 'Data fetched successfully',
-                    data: { follow: true }
-                });
-            }
-            else {
-                await following.destroy();
-                await followed.destroy();
-                res.status(201).send({
-                    error: false,
-                    message: 'Data fetched successfully',
-                    data: { follow: false }
-                });
-            }
+      if (following) {
+        const newFollowings = followings.filter((follow) => follow.id !== otherUser.id);
+        // const otherFollowers = await otherUser.getFollower();
+        // const newOtherFollowers = await otherFollowers.filter((follow) => follow.id !== user.id);
 
+        await user.setFollowed(newFollowings);
+        // await otherUser.setFollower(newOtherFollowers);
 
-        } catch (err) {
-            res.status(500).send({ error: true, data: [], message: err.message });
-        }
-    } else {
-        res.status(404).end('Page Not Found');
+        res.status(201).send({
+          error: false,
+          message: 'Data fetched successfully',
+          data: { follow: false }
+        });
+      } else {
+        await user.addFollowed(otherUser);
+        // await otherUser.addFollower(user);
+
+        res.status(201).send({
+          error: false,
+          message: 'Data fetched successfully',
+          data: { follow: true }
+        });
+      }
+    } catch (err) {
+      res.status(500).send({ error: true, data: [], message: err.message });
     }
+  } else {
+    res.status(404).end('Page Not Found');
+  }
 };
 
 export default handler;
