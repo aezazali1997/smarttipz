29ad58f5e7ae1df272fdf59022aba558ea74e0fe
aezazali1997 <aezazali.pro@gmail.com>
2022-02-18 21:01:11 +0500

    issue resolved

--- src/pages/search.js
+++ src/pages/search.js
@@ -195,7 +195,7 @@ const Search = () => {
 														videoType={videoType}
 														watchLimit={watchLimit}
 														productLink={productLink}
-														avgRating={avgRating}
+														// avgRating={avgRating}
 														width={'max-w-lg'}
 
 														thumbnail={thumbnail}
