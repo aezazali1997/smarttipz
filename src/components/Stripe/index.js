f9b0354eb45583d65ac28b29e3deefa54c8b2128
aezazali1997 <aezazali.pro@gmail.com>
2022-03-31 10:29:52 +0500

    stripe

--- src/components/Stripe/index.js
+++ src/components/Stripe/index.js
@@ -26,27 +26,26 @@ const Stripe = ({ setIsVerified, setCardToken, isVerified }) => {
       makePayment();
     }
   }, [stripe]);
-  const makePayment = async () => {
-    const pr = stripe.paymentRequest({
-      country: 'US',
-      currency: 'usd',
-      total: {
-        label: 'Demo total',
-        amount: 1099
-      },
-      requestPayerName: true,
-      requestPayerEmail: true
-    });
-
-    pr.canMakePayment().then((result) => {
-      console.log('result', result);
-      if (result) {
-        setPaymentRequest(result);
-        console.log(result);
-      } else {
-        console.log('no result');
-      }
-    });
+  const makePayment = () => {
+    // const pr = stripe.paymentRequest({
+    //   country: 'US',
+    //   currency: 'usd',
+    //   total: {
+    //     label: 'Demo total',
+    //     amount: 1099
+    //   },
+    //   requestPayerName: true,
+    //   requestPayerEmail: true
+    // });
+    // pr.canMakePayment().then((result) => {
+    //   console.log('result', result);
+    //   if (result) {
+    //     setPaymentRequest(result);
+    //     console.log(result);
+    //   } else {
+    //     console.log('no result');
+    //   }
+    // });
   };
 
   if (paymentRequest) {
